4b13bbc036cb549c81c91bc020804a4036324084
Dominik Marks <Dominik.Marks@gmx.de>
2018-06-01 09:07:04 +0200

    SF and copyright
    Signed-off-by: Dominik Marks <Dominik.Marks@gmx.de>

--- src/main/java/com/liferay/ide/idea/language/LiferayXmlSchemaProvider.java
+++ src/main/java/com/liferay/ide/idea/language/LiferayXmlSchemaProvider.java
@@ -1,9 +1,24 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
 package com.liferay.ide.idea.language;
 
 import com.intellij.ide.highlighter.XmlFileType;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.vfs.VfsUtil;
 import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.psi.PsiDirectory;
 import com.intellij.psi.PsiFile;
 import com.intellij.psi.PsiManager;
 import com.intellij.psi.xml.XmlFile;
@@ -32,25 +47,32 @@ public class LiferayXmlSchemaProvider extends XmlSchemaProvider {
 			psiFile = baseFile.getOriginalFile();
 		}
 
-		if (psiFile.getName().equals("portlet-model-hints.xml")) {
-			targetFileUrl = LiferayXmlSchemaProvider.class.getResource(
-			        "/xsd/liferay-portlet-model-hints_7_0_0.xsd"
-            );
-		} else if (
-				(psiFile.getName().equals("default.xml")) &&
-                (psiFile.getParent() != null) &&
-                (psiFile.getParent().getName().equals("custom-sql"))
-        ) {
-			targetFileUrl = LiferayXmlSchemaProvider.class.getResource(
-			        "/xsd/liferay-custom-sql_7_0_0.xsd"
-            );
+		String fileName = psiFile.getName();
+
+		if ("portlet-model-hints.xml".equals(fileName)) {
+			targetFileUrl = LiferayXmlSchemaProvider.class.getResource("/xsd/liferay-portlet-model-hints_7_0_0.xsd");
+		}
+		else {
+			if ("default.xml".equals(fileName)) {
+				PsiDirectory parentDirectory = psiFile.getParent();
+
+				if (parentDirectory != null) {
+					String parentDirectoryName = parentDirectory.getName();
+
+					if ("custom-sql".equals(parentDirectoryName)) {
+						targetFileUrl = LiferayXmlSchemaProvider.class.getResource("/xsd/liferay-custom-sql_7_0_0.xsd");
+					}
+				}
+			}
 		}
 
 		if (targetFileUrl != null) {
 			VirtualFile virtualFile = VfsUtil.findFileByURL(targetFileUrl);
 
 			if (virtualFile != null) {
-				PsiFile targetFile = PsiManager.getInstance(baseFile.getProject()).findFile(virtualFile);
+				PsiManager psiManager = PsiManager.getInstance(baseFile.getProject());
+
+				PsiFile targetFile = psiManager.findFile(virtualFile);
 
 				if (targetFile instanceof XmlFile) {
 					return (XmlFile)targetFile;
@@ -73,13 +95,21 @@ public class LiferayXmlSchemaProvider extends XmlSchemaProvider {
 			return false;
 		}
 
-		if (psiFile.getName().equals("portlet-model-hints.xml")) {
+		String fileName = psiFile.getName();
+
+		if ("portlet-model-hints.xml".equals(fileName)) {
 			return true;
 		}
 
-		if (psiFile.getName().equals("default.xml")) {
-			if ((psiFile.getParent() != null) && (psiFile.getParent().getName().equals("custom-sql"))) {
-				return true;
+		if ("default.xml".equals(fileName)) {
+			PsiDirectory parentDirectory = psiFile.getParent();
+
+			if (parentDirectory != null) {
+				String parentDirectoryName = parentDirectory.getName();
+
+				if ("custom-sql".equals(parentDirectoryName)) {
+					return true;
+				}
 			}
 		}
 
