3e1ac32f257efa23b8bcc414baa9ce390c6b225e
simon.jiang <simon.jiang@liferay.com>
2018-07-12 13:24:47 +0800

    IDE-4125 add projet view decortator for intellij

--- src/main/java/com/liferay/ide/idea/server/LiferayServerDebugger.java
+++ src/main/java/com/liferay/ide/idea/server/LiferayServerDebugger.java
@@ -15,13 +15,27 @@
 package com.liferay.ide.idea.server;
 
 import com.intellij.debugger.impl.GenericDebuggerRunner;
+import com.intellij.execution.ExecutionException;
+import com.intellij.execution.ExecutionManager;
 import com.intellij.execution.configurations.RunProfile;
+import com.intellij.execution.configurations.RunProfileState;
 import com.intellij.execution.executors.DefaultDebugExecutor;
+import com.intellij.execution.process.ProcessAdapter;
+import com.intellij.execution.process.ProcessEvent;
+import com.intellij.execution.process.ProcessHandler;
+import com.intellij.execution.runners.ExecutionEnvironment;
+import com.intellij.execution.ui.RunContentDescriptor;
+import com.intellij.openapi.externalSystem.service.execution.ExternalSystemProcessHandler;
+import com.intellij.openapi.project.Project;
+
+import java.util.stream.Stream;
 
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 /**
  * @author Terry Jia
+ * @author Simon Jiang
  */
 public class LiferayServerDebugger extends GenericDebuggerRunner {
 
@@ -38,6 +52,56 @@ public class LiferayServerDebugger extends GenericDebuggerRunner {
 	@Override
 	public String getRunnerId() {
 		return "LiferayBundleDebugger";
+	}
+
+	@Nullable
+	@Override
+	protected RunContentDescriptor doExecute(@NotNull RunProfileState state, @NotNull ExecutionEnvironment environment)
+		throws ExecutionException {
+
+		RunContentDescriptor descriptor = super.doExecute(state, environment);
+
+		if (descriptor != null) {
+			ProcessHandler processHandler = descriptor.getProcessHandler();
+
+			if (processHandler == null) {
+				return descriptor;
+			}
+
+			processHandler.addProcessListener(
+				new ProcessAdapter() {
+
+					@Override
+					public void processWillTerminate(@NotNull ProcessEvent event, boolean willBeDestroyed) {
+						Project project = environment.getProject();
+
+						ExecutionManager executionManager = ExecutionManager.getInstance(project);
+
+						ProcessHandler[] runningProcesses = executionManager.getRunningProcesses();
+
+						Stream.of(
+							runningProcesses
+						).filter(
+							process -> !process.equals(event.getProcessHandler())
+						).filter(
+							process -> process instanceof ExternalSystemProcessHandler
+						).forEach(
+							process -> {
+								ExternalSystemProcessHandler exProcessHandler = (ExternalSystemProcessHandler)process;
+
+								String executionName = exProcessHandler.getExecutionName();
+
+								if (executionName.contains("[watch]")) {
+									process.destroyProcess();
+								}
+							}
+						);
+					}
+
+				});
+		}
+
+		return descriptor;
 	}
 
 }
---
+++ src/main/java/com/liferay/ide/idea/ui/decorator/ProjectViewWatchDecorator.java
@@ -0,0 +1,96 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.ide.idea.ui.decorator;
+
+import com.intellij.ide.projectView.PresentationData;
+import com.intellij.ide.projectView.ProjectViewNode;
+import com.intellij.ide.projectView.ProjectViewNodeDecorator;
+import com.intellij.ide.projectView.impl.ProjectRootsUtil;
+import com.intellij.openapi.components.ServiceManager;
+import com.intellij.openapi.externalSystem.model.task.ExternalSystemTask;
+import com.intellij.openapi.externalSystem.model.task.ExternalSystemTaskState;
+import com.intellij.openapi.externalSystem.service.internal.ExternalSystemExecuteTaskTask;
+import com.intellij.openapi.externalSystem.service.internal.ExternalSystemProcessingManager;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.packageDependencies.ui.PackageDependenciesNode;
+import com.intellij.ui.ColoredTreeCellRenderer;
+
+import com.liferay.ide.idea.util.CoreUtil;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import org.jetbrains.plugins.gradle.util.GradleConstants;
+
+/**
+ * @author Simon Jiang
+ */
+public class ProjectViewWatchDecorator implements ProjectViewNodeDecorator {
+
+	@Override
+	public void decorate(PackageDependenciesNode node, ColoredTreeCellRenderer cellRenderer) {
+	}
+
+	@Override
+	public void decorate(ProjectViewNode node, PresentationData data) {
+		final VirtualFile virtualFile = node.getVirtualFile();
+		Project project = node.getProject();
+
+		if (project == null) {
+			return;
+		}
+
+		if ((virtualFile != null) && ProjectRootsUtil.isModuleContentRoot(virtualFile, project)) {
+			String canonicalPath = virtualFile.getCanonicalPath();
+
+			ExternalSystemProcessingManager processingManager = ServiceManager.getService(
+				ExternalSystemProcessingManager.class);
+
+			List<ExternalSystemTask> taskList = processingManager.findTasksOfState(
+				GradleConstants.SYSTEM_ID, ExternalSystemTaskState.IN_PROGRESS);
+
+			Stream<ExternalSystemTask> taskStream = taskList.stream();
+
+			taskStream.filter(
+				task -> task != null
+			).filter(
+				task -> task instanceof ExternalSystemExecuteTaskTask
+			).forEach(
+				task -> {
+					ExternalSystemExecuteTaskTask executeTask = (ExternalSystemExecuteTaskTask)task;
+
+					List<String> tasksToExecute = executeTask.getTasksToExecute();
+
+					if (tasksToExecute.contains("watch")) {
+						String externalProjectPath = executeTask.getExternalProjectPath();
+
+						if (!CoreUtil.isNullOrEmpty(canonicalPath) && canonicalPath.equals(externalProjectPath)) {
+							String existedLocationString = data.getLocationString();
+
+							if (existedLocationString != null) {
+								data.setLocationString(existedLocationString + " [watching]");
+							}
+							else {
+								data.setLocationString("[watching]");
+							}
+						}
+					}
+				}
+			);
+		}
+	}
+
+}
