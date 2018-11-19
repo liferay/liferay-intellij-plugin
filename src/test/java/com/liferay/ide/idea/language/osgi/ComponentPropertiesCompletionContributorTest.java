566de5e6b54e4505965e048ea7403a3c27c2097a
Dominik Marks <Dominik.Marks@gmx.de>
2018-11-19 09:56:06 +0100

    INTELLIJ-23 SF
    Signed-off-by: Dominik Marks <Dominik.Marks@gmx.de>

--- src/test/java/com/liferay/ide/idea/language/osgi/ComponentPropertiesCompletionContributorTest.java
+++ src/test/java/com/liferay/ide/idea/language/osgi/ComponentPropertiesCompletionContributorTest.java
@@ -1,6 +1,22 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
 package com.liferay.ide.idea.language.osgi;
 
 import com.intellij.codeInsight.completion.CompletionType;
+import com.intellij.openapi.module.Module;
+import com.intellij.openapi.projectRoots.Sdk;
 import com.intellij.openapi.projectRoots.impl.JavaAwareProjectJdkTableImpl;
 import com.intellij.openapi.roots.ContentEntry;
 import com.intellij.openapi.roots.LanguageLevelModuleExtension;
@@ -12,55 +28,70 @@ import com.intellij.testFramework.PsiTestUtil;
 import com.intellij.testFramework.fixtures.DefaultLightProjectDescriptor;
 import com.intellij.testFramework.fixtures.LightCodeInsightFixtureTestCase;
 import com.intellij.util.PathUtil;
-import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
+
 import java.util.List;
 
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * @author Dominik Marks
+ */
 public class ComponentPropertiesCompletionContributorTest extends LightCodeInsightFixtureTestCase {
 
-    private static final String TEST_DATA_PATH = "testdata/com/liferay/ide/idea/language/osgi/ComponentPropertiesCompletionContributorTest";
+	public void testComponentPropertiesCompletion() {
+		myFixture.configureByFile("MyComponent.java");
 
-    private static final LightProjectDescriptor JAVA_OSGI_LIB_DESCRIPTOR = new DefaultLightProjectDescriptor() {
+		myFixture.complete(CompletionType.BASIC, 1);
 
-        @Override
-        public void configureModule(@NotNull com.intellij.openapi.module.Module module, @NotNull ModifiableRootModel model, @NotNull ContentEntry contentEntry) {
-            LanguageLevelModuleExtension extension = model.getModuleExtension(LanguageLevelModuleExtension.class);
-            if (extension != null) {
-                extension.setLanguageLevel(LanguageLevel.JDK_1_8);
-            }
-            com.intellij.openapi.projectRoots.Sdk jdk = JavaAwareProjectJdkTableImpl.getInstanceEx().getInternalJdk();
-            model.setSdk(jdk);
+		List<String> strings = myFixture.getLookupElementStrings();
 
-            final String testDataPath = PathUtil.toSystemIndependentName(new File(TEST_DATA_PATH).getAbsolutePath());
-            VfsRootAccess.allowRootAccess( testDataPath );
+		assertTrue(strings.contains("osgi.command.scope"));
+	}
 
-            PsiTestUtil.addLibrary(module, model, "OSGi", testDataPath, "osgi.jar");
-        }
-    };
+	@NotNull
+	@Override
+	protected LightProjectDescriptor getProjectDescriptor() {
+		return _JAVA_OSGI_LIB_DESCRIPTOR;
+	}
 
-    @Override
-    protected void setUp() throws Exception {
-        super.setUp();
-    }
+	@Override
+	protected String getTestDataPath() {
+		return _TEST_DATA_PATH;
+	}
 
-    @NotNull
-    @Override
-    protected LightProjectDescriptor getProjectDescriptor() {
-        return JAVA_OSGI_LIB_DESCRIPTOR;
-    }
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
 
-    @Override
-    protected String getTestDataPath() {
-        return TEST_DATA_PATH;
-    }
+	private static final LightProjectDescriptor _JAVA_OSGI_LIB_DESCRIPTOR = new DefaultLightProjectDescriptor() {
 
-    public void testOsgiImplicitUsage() {
-        myFixture.configureByFile("MyComponent.java");
+		@Override
+		public void configureModule(
+			@NotNull Module module, @NotNull ModifiableRootModel model, @NotNull ContentEntry contentEntry) {
 
-        myFixture.complete(CompletionType.BASIC, 1);
-        List<String> strings = myFixture.getLookupElementStrings();
-        assertTrue(strings.contains("osgi.command.scope"));
-    }
+			LanguageLevelModuleExtension extension = model.getModuleExtension(LanguageLevelModuleExtension.class);
+
+			if (extension != null) {
+				extension.setLanguageLevel(LanguageLevel.JDK_1_8);
+			}
+
+			Sdk internalJdk = JavaAwareProjectJdkTableImpl.getInstanceEx().getInternalJdk();
+
+			model.setSdk(internalJdk);
+
+			final String testDataPath = PathUtil.toSystemIndependentName(new File(_TEST_DATA_PATH).getAbsolutePath());
+
+			VfsRootAccess.allowRootAccess(testDataPath);
+
+			PsiTestUtil.addLibrary(module, model, "OSGi", testDataPath, "osgi.jar");
+		}
+
+	};
+
+	private static final String _TEST_DATA_PATH =
+		"testdata/com/liferay/ide/idea/language/osgi/ComponentPropertiesCompletionContributorTest";
 
 }
