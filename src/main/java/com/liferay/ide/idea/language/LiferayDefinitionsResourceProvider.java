1b3d026ffcf864a3fbb92c479a9ef547b75d5efd
Dominik Marks <Dominik.Marks@gmx.de>
2019-07-16 21:53:28 +0200

    INTELLIJ-78 added Liferay 7.2 DTDs and XSDs
    Signed-off-by: Dominik Marks <Dominik.Marks@gmx.de>

--- src/main/java/com/liferay/ide/idea/language/LiferayDefinitionsResourceProvider.java
+++ src/main/java/com/liferay/ide/idea/language/LiferayDefinitionsResourceProvider.java
@@ -31,15 +31,23 @@ public class LiferayDefinitionsResourceProvider implements StandardResourceProvi
 	public static final String XML_NAMESPACE_LIFERAY_DISPLAY_7_1_0 =
 		"http://www.liferay.com/dtd/liferay-display_7_1_0.dtd";
 
+	public static final String XML_NAMESPACE_LIFERAY_DISPLAY_7_2_0 =
+		"http://www.liferay.com/dtd/liferay-display_7_2_0.dtd";
+
 	public static final String XML_NAMESPACE_LIFERAY_FRIENDLY_URL_ROUTES_7_0_0 =
 		"http://www.liferay.com/dtd/liferay-friendly-url-routes_7_0_0.dtd";
 
 	public static final String XML_NAMESPACE_LIFERAY_FRIENDLY_URL_ROUTES_7_1_0 =
 		"http://www.liferay.com/dtd/liferay-friendly-url-routes_7_1_0.dtd";
 
+	public static final String XML_NAMESPACE_LIFERAY_FRIENDLY_URL_ROUTES_7_2_0 =
+		"http://www.liferay.com/dtd/liferay-friendly-url-routes_7_2_0.dtd";
+
 	public static final String XML_NAMESPACE_LIFERAY_HOOK_7_0_0 = "http://www.liferay.com/dtd/liferay-hook_7_0_0.dtd";
 
 	public static final String XML_NAMESPACE_LIFERAY_HOOK_7_1_0 = "http://www.liferay.com/dtd/liferay-hook_7_1_0.dtd";
+
+	public static final String XML_NAMESPACE_LIFERAY_HOOK_7_2_0 = "http://www.liferay.com/dtd/liferay-hook_7_2_0.dtd";
 
 	public static final String XML_NAMESPACE_LIFERAY_LAYOUT_TEMPLATES_7_0_0 =
 		"http://www.liferay.com/dtd/liferay-layout-templates_7_0_0.dtd";
@@ -47,11 +55,17 @@ public class LiferayDefinitionsResourceProvider implements StandardResourceProvi
 	public static final String XML_NAMESPACE_LIFERAY_LAYOUT_TEMPLATES_7_1_0 =
 		"http://www.liferay.com/dtd/liferay-layout-templates_7_1_0.dtd";
 
+	public static final String XML_NAMESPACE_LIFERAY_LAYOUT_TEMPLATES_7_2_0 =
+		"http://www.liferay.com/dtd/liferay-layout-templates_7_2_0.dtd";
+
 	public static final String XML_NAMESPACE_LIFERAY_LOOK_AND_FEEL_7_0_0 =
 		"http://www.liferay.com/dtd/liferay-look-and-feel_7_0_0.dtd";
 
 	public static final String XML_NAMESPACE_LIFERAY_LOOK_AND_FEEL_7_1_0 =
 		"http://www.liferay.com/dtd/liferay-look-and-feel_7_1_0.dtd";
+
+	public static final String XML_NAMESPACE_LIFERAY_LOOK_AND_FEEL_7_2_0 =
+		"http://www.liferay.com/dtd/liferay-look-and-feel_7_2_0.dtd";
 
 	public static final String XML_NAMESPACE_LIFERAY_PLUGIN_PACKAGE_7_0_0 =
 		"http://www.liferay.com/dtd/liferay-plugin-package_7_0_0.dtd";
@@ -59,11 +73,17 @@ public class LiferayDefinitionsResourceProvider implements StandardResourceProvi
 	public static final String XML_NAMESPACE_LIFERAY_PLUGIN_PACKAGE_7_1_0 =
 		"http://www.liferay.com/dtd/liferay-plugin-package_7_1_0.dtd";
 
+	public static final String XML_NAMESPACE_LIFERAY_PLUGIN_PACKAGE_7_2_0 =
+		"http://www.liferay.com/dtd/liferay-plugin-package_7_2_0.dtd";
+
 	public static final String XML_NAMESPACE_LIFERAY_PLUGIN_REPOSITORY_7_0_0 =
 		"http://www.liferay.com/dtd/liferay-plugin-repository_7_0_0.dtd";
 
 	public static final String XML_NAMESPACE_LIFERAY_PLUGIN_REPOSITORY_7_1_0 =
 		"http://www.liferay.com/dtd/liferay-plugin-repository_7_1_0.dtd";
+
+	public static final String XML_NAMESPACE_LIFERAY_PLUGIN_REPOSITORY_7_2_0 =
+		"http://www.liferay.com/dtd/liferay-plugin-repository_7_2_0.dtd";
 
 	public static final String XML_NAMESPACE_LIFERAY_PORTLET_APP_7_0_0 =
 		"http://www.liferay.com/dtd/liferay-portlet-app_7_0_0.dtd";
@@ -71,11 +91,17 @@ public class LiferayDefinitionsResourceProvider implements StandardResourceProvi
 	public static final String XML_NAMESPACE_LIFERAY_PORTLET_APP_7_1_0 =
 		"http://www.liferay.com/dtd/liferay-portlet-app_7_1_0.dtd";
 
+	public static final String XML_NAMESPACE_LIFERAY_PORTLET_APP_7_2_0 =
+		"http://www.liferay.com/dtd/liferay-portlet-app_7_2_0.dtd";
+
 	public static final String XML_NAMESPACE_LIFERAY_RESOURCE_ACTION_MAPPING_7_0_0 =
 		"http://www.liferay.com/dtd/liferay-resource-action-mapping_7_0_0.dtd";
 
 	public static final String XML_NAMESPACE_LIFERAY_RESOURCE_ACTION_MAPPING_7_1_0 =
 		"http://www.liferay.com/dtd/liferay-resource-action-mapping_7_1_0.dtd";
+
+	public static final String XML_NAMESPACE_LIFERAY_RESOURCE_ACTION_MAPPING_7_2_0 =
+		"http://www.liferay.com/dtd/liferay-resource-action-mapping_7_2_0.dtd";
 
 	public static final String XML_NAMESPACE_LIFERAY_SERVICE_BUILDER_7_0_0 =
 		"http://www.liferay.com/dtd/liferay-service-builder_7_0_0.dtd";
@@ -83,11 +109,17 @@ public class LiferayDefinitionsResourceProvider implements StandardResourceProvi
 	public static final String XML_NAMESPACE_LIFERAY_SERVICE_BUILDER_7_1_0 =
 		"http://www.liferay.com/dtd/liferay-service-builder_7_1_0.dtd";
 
+	public static final String XML_NAMESPACE_LIFERAY_SERVICE_BUILDER_7_2_0 =
+		"http://www.liferay.com/dtd/liferay-service-builder_7_2_0.dtd";
+
 	public static final String XML_NAMESPACE_LIFERAY_SOCIAL_7_0_0 =
 		"http://www.liferay.com/dtd/liferay-social_7_0_0.dtd";
 
 	public static final String XML_NAMESPACE_LIFERAY_SOCIAL_7_1_0 =
 		"http://www.liferay.com/dtd/liferay-social_7_1_0.dtd";
+
+	public static final String XML_NAMESPACE_LIFERAY_SOCIAL_7_2_0 =
+		"http://www.liferay.com/dtd/liferay-social_7_2_0.dtd";
 
 	public static final String XML_NAMESPACE_LIFERAY_THEME_LOADER_7_0_0 =
 		"http://www.liferay.com/dtd/liferay-theme-loader_7_0_0.dtd";
@@ -95,17 +127,26 @@ public class LiferayDefinitionsResourceProvider implements StandardResourceProvi
 	public static final String XML_NAMESPACE_LIFERAY_THEME_LOADER_7_1_0 =
 		"http://www.liferay.com/dtd/liferay-theme-loader_7_1_0.dtd";
 
+	public static final String XML_NAMESPACE_LIFERAY_THEME_LOADER_7_2_0 =
+		"http://www.liferay.com/dtd/liferay-theme-loader_7_2_0.dtd";
+
 	public static final String XML_NAMESPACE_LIFERAY_USER_NOTIFICATION_DEFINITIONS_7_0_0 =
 		"http://www.liferay.com/dtd/liferay-user-notification-definitions_7_0_0.dtd";
 
 	public static final String XML_NAMESPACE_LIFERAY_USER_NOTIFICATION_DEFINITIONS_7_1_0 =
 		"http://www.liferay.com/dtd/liferay-user-notification-definitions_7_1_0.dtd";
 
+	public static final String XML_NAMESPACE_LIFERAY_USER_NOTIFICATION_DEFINITIONS_7_2_0 =
+		"http://www.liferay.com/dtd/liferay-user-notification-definitions_7_2_0.dtd";
+
 	public static final String XML_NAMESPACE_LIFERAY_WORKFLOW_DEFINITION_7_0_0 =
 		"http://www.liferay.com/dtd/liferay-workflow-definition_7_0_0.xsd";
 
 	public static final String XML_NAMESPACE_LIFERAY_WORKFLOW_DEFINITION_7_1_0 =
 		"http://www.liferay.com/dtd/liferay-workflow-definition_7_1_0.xsd";
+
+	public static final String XML_NAMESPACE_LIFERAY_WORKFLOW_DEFINITION_7_2_0 =
+		"http://www.liferay.com/dtd/liferay-workflow-definition_7_2_0.xsd";
 
 	public static final String XML_NAMESPACE_SUN_PORTLET_APP_2_0 =
 		"http://java.sun.com/xml/ns/portlet/portlet-app_2_0.xsd";
@@ -118,10 +159,16 @@ public class LiferayDefinitionsResourceProvider implements StandardResourceProvi
 			XML_NAMESPACE_LIFERAY_DISPLAY_7_1_0, "/definitions/dtd/liferay-display_7_1_0.dtd",
 			LiferayDefinitionsResourceProvider.class);
 		resourceRegistrar.addStdResource(
+			XML_NAMESPACE_LIFERAY_DISPLAY_7_2_0, "/definitions/dtd/liferay-display_7_2_0.dtd",
+			LiferayDefinitionsResourceProvider.class);
+		resourceRegistrar.addStdResource(
 			XML_NAMESPACE_LIFERAY_FRIENDLY_URL_ROUTES_7_0_0, "/definitions/dtd/liferay-friendly-url-routes_7_0_0.dtd",
 			LiferayDefinitionsResourceProvider.class);
 		resourceRegistrar.addStdResource(
 			XML_NAMESPACE_LIFERAY_FRIENDLY_URL_ROUTES_7_1_0, "/definitions/dtd/liferay-friendly-url-routes_7_1_0.dtd",
+			LiferayDefinitionsResourceProvider.class);
+		resourceRegistrar.addStdResource(
+			XML_NAMESPACE_LIFERAY_FRIENDLY_URL_ROUTES_7_2_0, "/definitions/dtd/liferay-friendly-url-routes_7_2_0.dtd",
 			LiferayDefinitionsResourceProvider.class);
 		resourceRegistrar.addStdResource(
 			XML_NAMESPACE_LIFERAY_HOOK_7_0_0, "/definitions/dtd/liferay-hook_7_0_0.dtd",
@@ -130,10 +177,16 @@ public class LiferayDefinitionsResourceProvider implements StandardResourceProvi
 			XML_NAMESPACE_LIFERAY_HOOK_7_1_0, "/definitions/dtd/liferay-hook_7_1_0.dtd",
 			LiferayDefinitionsResourceProvider.class);
 		resourceRegistrar.addStdResource(
+			XML_NAMESPACE_LIFERAY_HOOK_7_2_0, "/definitions/dtd/liferay-hook_7_2_0.dtd",
+			LiferayDefinitionsResourceProvider.class);
+		resourceRegistrar.addStdResource(
 			XML_NAMESPACE_LIFERAY_LAYOUT_TEMPLATES_7_0_0, "/definitions/dtd/liferay-layout-templates_7_0_0.dtd",
 			LiferayDefinitionsResourceProvider.class);
 		resourceRegistrar.addStdResource(
 			XML_NAMESPACE_LIFERAY_LAYOUT_TEMPLATES_7_1_0, "/definitions/dtd/liferay-layout-templates_7_1_0.dtd",
+			LiferayDefinitionsResourceProvider.class);
+		resourceRegistrar.addStdResource(
+			XML_NAMESPACE_LIFERAY_LAYOUT_TEMPLATES_7_2_0, "/definitions/dtd/liferay-layout-templates_7_2_0.dtd",
 			LiferayDefinitionsResourceProvider.class);
 		resourceRegistrar.addStdResource(
 			XML_NAMESPACE_LIFERAY_LOOK_AND_FEEL_7_0_0, "/definitions/dtd/liferay-look-and-feel_7_0_0.dtd",
@@ -142,10 +195,16 @@ public class LiferayDefinitionsResourceProvider implements StandardResourceProvi
 			XML_NAMESPACE_LIFERAY_LOOK_AND_FEEL_7_1_0, "/definitions/dtd/liferay-look-and-feel_7_1_0.dtd",
 			LiferayDefinitionsResourceProvider.class);
 		resourceRegistrar.addStdResource(
+			XML_NAMESPACE_LIFERAY_LOOK_AND_FEEL_7_2_0, "/definitions/dtd/liferay-look-and-feel_7_2_0.dtd",
+			LiferayDefinitionsResourceProvider.class);
+		resourceRegistrar.addStdResource(
 			XML_NAMESPACE_LIFERAY_PLUGIN_PACKAGE_7_0_0, "/definitions/dtd/liferay-plugin-package_7_0_0.dtd",
 			LiferayDefinitionsResourceProvider.class);
 		resourceRegistrar.addStdResource(
 			XML_NAMESPACE_LIFERAY_PLUGIN_PACKAGE_7_1_0, "/definitions/dtd/liferay-plugin-package_7_1_0.dtd",
+			LiferayDefinitionsResourceProvider.class);
+		resourceRegistrar.addStdResource(
+			XML_NAMESPACE_LIFERAY_PLUGIN_PACKAGE_7_2_0, "/definitions/dtd/liferay-plugin-package_7_2_0.dtd",
 			LiferayDefinitionsResourceProvider.class);
 		resourceRegistrar.addStdResource(
 			XML_NAMESPACE_LIFERAY_PLUGIN_REPOSITORY_7_0_0, "/definitions/dtd/liferay-plugin-repository_7_0_0.dtd",
@@ -154,10 +213,16 @@ public class LiferayDefinitionsResourceProvider implements StandardResourceProvi
 			XML_NAMESPACE_LIFERAY_PLUGIN_REPOSITORY_7_1_0, "/definitions/dtd/liferay-plugin-repository_7_1_0.dtd",
 			LiferayDefinitionsResourceProvider.class);
 		resourceRegistrar.addStdResource(
+			XML_NAMESPACE_LIFERAY_PLUGIN_REPOSITORY_7_2_0, "/definitions/dtd/liferay-plugin-repository_7_2_0.dtd",
+			LiferayDefinitionsResourceProvider.class);
+		resourceRegistrar.addStdResource(
 			XML_NAMESPACE_LIFERAY_PORTLET_APP_7_0_0, "/definitions/dtd/liferay-portlet-app_7_0_0.dtd",
 			LiferayDefinitionsResourceProvider.class);
 		resourceRegistrar.addStdResource(
 			XML_NAMESPACE_LIFERAY_PORTLET_APP_7_1_0, "/definitions/dtd/liferay-portlet-app_7_1_0.dtd",
+			LiferayDefinitionsResourceProvider.class);
+		resourceRegistrar.addStdResource(
+			XML_NAMESPACE_LIFERAY_PORTLET_APP_7_2_0, "/definitions/dtd/liferay-portlet-app_7_2_0.dtd",
 			LiferayDefinitionsResourceProvider.class);
 		resourceRegistrar.addStdResource(
 			XML_NAMESPACE_LIFERAY_RESOURCE_ACTION_MAPPING_7_0_0,
@@ -166,10 +231,16 @@ public class LiferayDefinitionsResourceProvider implements StandardResourceProvi
 			XML_NAMESPACE_LIFERAY_RESOURCE_ACTION_MAPPING_7_1_0,
 			"/definitions/dtd/liferay-resource-action-mapping_7_1_0.dtd", LiferayDefinitionsResourceProvider.class);
 		resourceRegistrar.addStdResource(
+			XML_NAMESPACE_LIFERAY_RESOURCE_ACTION_MAPPING_7_2_0,
+			"/definitions/dtd/liferay-resource-action-mapping_7_2_0.dtd", LiferayDefinitionsResourceProvider.class);
+		resourceRegistrar.addStdResource(
 			XML_NAMESPACE_LIFERAY_SERVICE_BUILDER_7_0_0, "/definitions/dtd/liferay-service-builder_7_0_0.dtd",
 			LiferayDefinitionsResourceProvider.class);
 		resourceRegistrar.addStdResource(
 			XML_NAMESPACE_LIFERAY_SERVICE_BUILDER_7_1_0, "/definitions/dtd/liferay-service-builder_7_1_0.dtd",
+			LiferayDefinitionsResourceProvider.class);
+		resourceRegistrar.addStdResource(
+			XML_NAMESPACE_LIFERAY_SERVICE_BUILDER_7_2_0, "/definitions/dtd/liferay-service-builder_7_2_0.dtd",
 			LiferayDefinitionsResourceProvider.class);
 		resourceRegistrar.addStdResource(
 			XML_NAMESPACE_LIFERAY_SOCIAL_7_0_0, "/definitions/dtd/liferay-social_7_0_0.dtd",
@@ -178,10 +249,16 @@ public class LiferayDefinitionsResourceProvider implements StandardResourceProvi
 			XML_NAMESPACE_LIFERAY_SOCIAL_7_1_0, "/definitions/dtd/liferay-social_7_1_0.dtd",
 			LiferayDefinitionsResourceProvider.class);
 		resourceRegistrar.addStdResource(
+			XML_NAMESPACE_LIFERAY_SOCIAL_7_2_0, "/definitions/dtd/liferay-social_7_2_0.dtd",
+			LiferayDefinitionsResourceProvider.class);
+		resourceRegistrar.addStdResource(
 			XML_NAMESPACE_LIFERAY_THEME_LOADER_7_0_0, "/definitions/dtd/liferay-theme-loader_7_0_0.dtd",
 			LiferayDefinitionsResourceProvider.class);
 		resourceRegistrar.addStdResource(
 			XML_NAMESPACE_LIFERAY_THEME_LOADER_7_1_0, "/definitions/dtd/liferay-theme-loader_7_1_0.dtd",
+			LiferayDefinitionsResourceProvider.class);
+		resourceRegistrar.addStdResource(
+			XML_NAMESPACE_LIFERAY_THEME_LOADER_7_2_0, "/definitions/dtd/liferay-theme-loader_7_2_0.dtd",
 			LiferayDefinitionsResourceProvider.class);
 		resourceRegistrar.addStdResource(
 			XML_NAMESPACE_LIFERAY_USER_NOTIFICATION_DEFINITIONS_7_0_0,
@@ -192,10 +269,17 @@ public class LiferayDefinitionsResourceProvider implements StandardResourceProvi
 			"/definitions/dtd/liferay-user-notification-definitions_7_1_0.dtd",
 			LiferayDefinitionsResourceProvider.class);
 		resourceRegistrar.addStdResource(
+			XML_NAMESPACE_LIFERAY_USER_NOTIFICATION_DEFINITIONS_7_2_0,
+			"/definitions/dtd/liferay-user-notification-definitions_7_2_0.dtd",
+			LiferayDefinitionsResourceProvider.class);
+		resourceRegistrar.addStdResource(
 			XML_NAMESPACE_LIFERAY_WORKFLOW_DEFINITION_7_0_0, "/definitions/xsd/liferay-workflow-definition_7_0_0.xsd",
 			LiferayDefinitionsResourceProvider.class);
 		resourceRegistrar.addStdResource(
 			XML_NAMESPACE_LIFERAY_WORKFLOW_DEFINITION_7_1_0, "/definitions/xsd/liferay-workflow-definition_7_1_0.xsd",
+			LiferayDefinitionsResourceProvider.class);
+		resourceRegistrar.addStdResource(
+			XML_NAMESPACE_LIFERAY_WORKFLOW_DEFINITION_7_2_0, "/definitions/xsd/liferay-workflow-definition_7_2_0.xsd",
 			LiferayDefinitionsResourceProvider.class);
 		resourceRegistrar.addStdResource(
 			XML_NAMESPACE_SUN_PORTLET_APP_2_0, "/definitions/xsd/portlet-app_2_0.xsd",
