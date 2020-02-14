53484f836601eee871ee508e9c465de14f522bec
Dominik Marks <Dominik.Marks@gmx.de>
2020-02-14 13:25:29 +0100

    INTELLIJ-53 Pressing Enter in bnd.bnd file should wrap lines with a backslash

--- src/main/java/com/liferay/ide/idea/bnd/parser/BndHeaderParsers.java
+++ src/main/java/com/liferay/ide/idea/bnd/parser/BndHeaderParsers.java
@@ -14,12 +14,10 @@
 
 package com.liferay.ide.idea.bnd.parser;
 
-import com.liferay.ide.idea.bnd.LiferayBndConstants;
+import aQute.bnd.osgi.Constants;
 
 import java.util.HashMap;
 import java.util.Map;
-
-import org.gradle.internal.impldep.aQute.bnd.osgi.Constants;
 
 /**
  * @author Dominik Marks
@@ -36,15 +34,15 @@ public class BndHeaderParsers {
 			put(Constants.IMPORT_PACKAGE, BasePackageParser.INSTANCE);
 			put(Constants.PRIVATE_PACKAGE, BasePackageParser.INSTANCE);
 
-			for (String header : _CLASS_REFERENCE_PROPERTIES) {
+			for (String header : BndHeaderParsersConstants.CLASS_REFERENCE_PROPERTIES) {
 				put(header, ClassReferenceParser.INSTANCE);
 			}
 
-			for (String header : _FILE_REFERENCE_PROPERTIES) {
+			for (String header : BndHeaderParsersConstants.FILE_REFERENCE_PROPERTIES) {
 				put(header, FileReferenceParser.INSTANCE);
 			}
 
-			for (String header : _DEFAULT_HEADER_PROPERTIES) {
+			for (String header : BndHeaderParsersConstants.DEFAULT_HEADER_PROPERTIES) {
 				put(header, BndHeaderParser.INSTANCE);
 			}
 
@@ -60,24 +58,6 @@ public class BndHeaderParsers {
 				}
 			}
 		}
-	};
-
-	private static final String[] _CLASS_REFERENCE_PROPERTIES = {
-		LiferayBndConstants.PLUGIN_BUNDLE, LiferayBndConstants.PLUGIN_JSP, LiferayBndConstants.PLUGIN_NPM,
-		LiferayBndConstants.PLUGIN_RESOURCEBUNDLE, LiferayBndConstants.PLUGIN_SASS, LiferayBndConstants.PLUGIN_SERVICE,
-		LiferayBndConstants.PLUGIN_SPRING, aQute.bnd.osgi.Constants.TESTCASES
-	};
-
-	private static final String[] _DEFAULT_HEADER_PROPERTIES = {
-		LiferayBndConstants.JSP, LiferayBndConstants.SASS, LiferayBndConstants.LIFERAY_SERVICE_XML,
-		LiferayBndConstants.LIFERAY_RELENG_MODULE_GROUP_DESCRIPTION,
-		LiferayBndConstants.LIFERAY_RELENG_MODULE_GROUP_TITLE, LiferayBndConstants.LIFERAY_REQUIRE_SCHEMA_VERSION,
-		LiferayBndConstants.LIFERAY_SERVICE, LiferayBndConstants.DYNAMIC_IMPORT_PACKAGE,
-		LiferayBndConstants.LIFERAY_MODULES_COMPAT_ADAPTERS
-	};
-
-	private static final String[] _FILE_REFERENCE_PROPERTIES = {
-		LiferayBndConstants.LIFERAY_JS_CONFIG, LiferayBndConstants.LIFERAY_CONFIGURATION_PATH
 	};
 
 }
