b71767dcdfcb2bb9a5dcd6567582838a816b4b0a
Dominik Marks <Dominik.Marks@gmx.de>
2021-02-25 15:58:17 +0100

    INTELLIJ-169 implicit property usage for Liferay specific keys in gradle.properties
    Signed-off-by: Dominik Marks <Dominik.Marks@gmx.de>

---
+++ src/main/java/com/liferay/ide/idea/language/gradle/LiferayGradlePropertiesImplicitUsageProvider.java
@@ -0,0 +1,51 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.ide.idea.language.gradle;
+
+import com.intellij.codeInspection.unused.ImplicitPropertyUsageProvider;
+import com.intellij.lang.properties.psi.Property;
+import com.intellij.psi.PsiFile;
+
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * @author Dominik Marks
+ */
+public class LiferayGradlePropertiesImplicitUsageProvider extends ImplicitPropertyUsageProvider {
+
+	@Override
+	protected boolean isUsed(@NotNull Property property) {
+		PsiFile containingFile = property.getContainingFile();
+
+		String propertyKey = property.getKey();
+
+		if ((containingFile != null) && (propertyKey != null)) {
+			String fileName = containingFile.getName();
+
+			if (_GRADLE_PROPERTIES.equals(fileName) || _GRADLE_LOCAL_PROPERTIES.equals(fileName)) {
+				return propertyKey.startsWith(_LIFERAY_WORKSPACE_PREFIX);
+			}
+		}
+
+		return false;
+	}
+
+	private static final String _GRADLE_LOCAL_PROPERTIES = "gradle-local.properties";
+
+	private static final String _GRADLE_PROPERTIES = "gradle.properties";
+
+	private static final String _LIFERAY_WORKSPACE_PREFIX = "liferay.workspace.";
+
+}
---
+++ src/test/java/com/liferay/ide/idea/language/gradle/LiferayGradlePropertiesImplicitUsageProviderTest.java
@@ -0,0 +1,45 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.ide.idea.language.gradle;
+
+import com.intellij.codeInspection.unused.UnusedPropertyInspection;
+import com.intellij.testFramework.fixtures.BasePlatformTestCase;
+
+/**
+ * @author Dominik Marks
+ */
+public class LiferayGradlePropertiesImplicitUsageProviderTest extends BasePlatformTestCase {
+
+	public void testImplicitUsageJavaxPortletTitleInLanguageProperties() {
+		myFixture.configureByFiles("gradle.properties");
+
+		//Language.properties should not show any unused warning,
+		//even if javax.portlet.title.my_portlet is not used explicitly
+		myFixture.checkHighlighting();
+	}
+
+	@Override
+	protected String getTestDataPath() {
+		return "testdata/com/liferay/ide/idea/language/gradle/LiferayGradlePropertiesImplicitUsageProviderTest";
+	}
+
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+
+		myFixture.enableInspections(new UnusedPropertyInspection());
+	}
+
+}
