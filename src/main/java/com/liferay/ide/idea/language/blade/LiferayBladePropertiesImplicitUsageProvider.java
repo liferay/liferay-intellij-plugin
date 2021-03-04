a3f2919d65b13e0b6327a617f6b51c973c574d8e
Dominik Marks <Dominik.Marks@gmx.de>
2021-03-04 14:12:52 +0100

    INTELLIJ-169 implicit property usage for .blade.properties
    Signed-off-by: Dominik Marks <Dominik.Marks@gmx.de>

---
+++ src/main/java/com/liferay/ide/idea/language/blade/LiferayBladePropertiesImplicitUsageProvider.java
@@ -0,0 +1,45 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.ide.idea.language.blade;
+
+import com.intellij.codeInspection.unused.ImplicitPropertyUsageProvider;
+import com.intellij.lang.properties.psi.Property;
+import com.intellij.psi.PsiFile;
+
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * @author Dominik Marks
+ */
+public class LiferayBladePropertiesImplicitUsageProvider extends ImplicitPropertyUsageProvider {
+
+	@Override
+	protected boolean isUsed(@NotNull Property property) {
+		PsiFile containingFile = property.getContainingFile();
+
+		String propertyKey = property.getKey();
+
+		if ((containingFile != null) && (propertyKey != null)) {
+			String fileName = containingFile.getName();
+
+			return _BLADE_PROPERTIES.equals(fileName);
+		}
+
+		return false;
+	}
+
+	private static final String _BLADE_PROPERTIES = ".blade.properties";
+
+}
