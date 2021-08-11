3b3f5a40c47458c9deed3846610f4d1abd25aaed
Seiphon Wang <871358932@qq.com>
2021-08-11 16:54:46 +0800

    just for test, please ignore

--- src/test/java/com/liferay/ide/idea/language/javascript/LiferayJSPredefinedLibraryProviderTest.java
+++ src/test/java/com/liferay/ide/idea/language/javascript/LiferayJSPredefinedLibraryProviderTest.java
@@ -19,16 +19,13 @@ import com.intellij.openapi.externalSystem.model.project.LibraryData;
 import com.intellij.openapi.externalSystem.model.project.LibraryPathType;
 import com.intellij.testFramework.fixtures.LightJavaCodeInsightFixtureTestCase;
 import com.intellij.util.PathUtil;
-import com.intellij.util.ThrowableRunnable;
 
 import java.io.File;
-
-import java.lang.reflect.Field;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import org.jetbrains.annotations.NotNull;
+import org.apache.commons.lang.reflect.FieldUtils;
 import org.jetbrains.plugins.gradle.util.GradleConstants;
 
 /**
@@ -60,10 +57,9 @@ public class LiferayJSPredefinedLibraryProviderTest extends LightJavaCodeInsight
 
 		targetPlatformArtifacts.add(libraryData);
 
-		Field field = LiferayJSPredefinedLibraryProvider.class.getDeclaredField("_targetPlatformArtifacts");
+		Class<LiferayJSPredefinedLibraryProvider> clazz = LiferayJSPredefinedLibraryProvider.class;
 
-		field.setAccessible(true);
-		field.set(null, targetPlatformArtifacts);
+		FieldUtils.writeDeclaredStaticField(clazz, "_targetPlatformArtifacts", targetPlatformArtifacts, true);
 
 		super.setUp();
 	}
@@ -107,24 +103,6 @@ public class LiferayJSPredefinedLibraryProviderTest extends LightJavaCodeInsight
 	@Override
 	protected String getTestDataPath() {
 		return _TEST_DATA_PATH;
-	}
-
-	@Override
-	protected void runTestRunnable(@NotNull ThrowableRunnable<Throwable> testRunnable) throws Throwable {
-
-		// ignore all tests if not on windows
-
-		if (_isWindows()) {
-			super.runTestRunnable(testRunnable);
-		}
-	}
-
-	private boolean _isWindows() {
-		String osName = System.getProperty("os.name");
-
-		osName = osName.toLowerCase();
-
-		return osName.contains("windows");
 	}
 
 	private static final String _TEST_DATA_PATH =
