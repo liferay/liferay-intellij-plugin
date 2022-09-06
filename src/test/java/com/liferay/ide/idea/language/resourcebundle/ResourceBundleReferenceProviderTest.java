cf0f52f19e4ca688467d8329f56fe0ecc47b3564
Dominik Marks <Dominik.Marks@gmx.de>
2022-09-06 15:49:18 +0200

    INTELLIJ-251 use jdk11 for test, add dummy class ResourceBundle

--- src/test/java/com/liferay/ide/idea/language/resourcebundle/ResourceBundleReferenceProviderTest.java
+++ src/test/java/com/liferay/ide/idea/language/resourcebundle/ResourceBundleReferenceProviderTest.java
@@ -15,9 +15,20 @@
 package com.liferay.ide.idea.language.resourcebundle;
 
 import com.intellij.codeInsight.completion.CompletionType;
+import com.intellij.openapi.module.Module;
+import com.intellij.openapi.projectRoots.Sdk;
+import com.intellij.openapi.roots.ContentEntry;
+import com.intellij.openapi.roots.LanguageLevelModuleExtension;
+import com.intellij.openapi.roots.ModifiableRootModel;
+import com.intellij.pom.java.LanguageLevel;
+import com.intellij.testFramework.IdeaTestUtil;
+import com.intellij.testFramework.LightProjectDescriptor;
+import com.intellij.testFramework.fixtures.DefaultLightProjectDescriptor;
 import com.intellij.testFramework.fixtures.LightJavaCodeInsightFixtureTestCase;
 
 import java.util.List;
+
+import org.jetbrains.annotations.NotNull;
 
 /**
  * @author Dominik Marks
@@ -26,7 +37,8 @@ public class ResourceBundleReferenceProviderTest extends LightJavaCodeInsightFix
 
 	public void testResourceBundleCodeCompletion() {
 		myFixture.configureByFiles(
-			"MyResourceBundleLoader.java", "content/Language.properties", "content/Language_en.properties");
+			"MyResourceBundleLoader.java", "java/util/ResourceBundle.java", "content/Language.properties",
+			"content/Language_en.properties");
 		myFixture.complete(CompletionType.BASIC, 1);
 
 		List<String> strings = myFixture.getLookupElementStrings();
@@ -35,9 +47,37 @@ public class ResourceBundleReferenceProviderTest extends LightJavaCodeInsightFix
 		assertTrue(strings.contains("content.Language_en"));
 	}
 
+	@NotNull
+	@Override
+	protected LightProjectDescriptor getProjectDescriptor() {
+		return _lightProjectDescriptor;
+	}
+
 	@Override
 	protected String getTestDataPath() {
 		return "testdata/com/liferay/ide/idea/language/resourcebundle/ResourceBundleReferenceProviderTest";
 	}
+
+	private static final LightProjectDescriptor _lightProjectDescriptor = new DefaultLightProjectDescriptor() {
+
+		@Override
+		public void configureModule(
+			@NotNull Module module, @NotNull ModifiableRootModel modifiableRootModel,
+			@NotNull ContentEntry contentEntry) {
+
+			LanguageLevelModuleExtension languageLevelModuleExtension = modifiableRootModel.getModuleExtension(
+				LanguageLevelModuleExtension.class);
+
+			if (languageLevelModuleExtension != null) {
+				languageLevelModuleExtension.setLanguageLevel(LanguageLevel.JDK_11);
+			}
+		}
+
+		@Override
+		public Sdk getSdk() {
+			return IdeaTestUtil.getMockJdk(LanguageLevel.JDK_11.toJavaVersion());
+		}
+
+	};
 
 }
