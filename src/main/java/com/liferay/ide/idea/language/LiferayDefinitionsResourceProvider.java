3543e9ae0682f764805855c2e5389783b2906e48
Drew Brokke <drew.brokke@liferay.com>
2025-05-01 18:26:41 -0500

    LPD-53867 fixes deprecated usage - pass in classloader directly

--- src/main/java/com/liferay/ide/idea/language/LiferayDefinitionsResourceProvider.java
+++ src/main/java/com/liferay/ide/idea/language/LiferayDefinitionsResourceProvider.java
@@ -225,232 +225,197 @@ public class LiferayDefinitionsResourceProvider implements StandardResourceProvi
 		"http://java.sun.com/xml/ns/portlet/portlet-app_2_0.xsd";
 
 	public void registerResources(ResourceRegistrar resourceRegistrar) {
+		ClassLoader classLoader = LiferayDefinitionsResourceProvider.class.getClassLoader();
+
 		resourceRegistrar.addStdResource(
-			XML_NAMESPACE_LIFERAY_DISPLAY_7_0_0, "/definitions/dtd/liferay-display_7_0_0.dtd",
-			LiferayDefinitionsResourceProvider.class);
+			XML_NAMESPACE_LIFERAY_DISPLAY_7_0_0, "/definitions/dtd/liferay-display_7_0_0.dtd", classLoader);
 		resourceRegistrar.addStdResource(
-			XML_NAMESPACE_LIFERAY_DISPLAY_7_1_0, "/definitions/dtd/liferay-display_7_1_0.dtd",
-			LiferayDefinitionsResourceProvider.class);
+			XML_NAMESPACE_LIFERAY_DISPLAY_7_1_0, "/definitions/dtd/liferay-display_7_1_0.dtd", classLoader);
 		resourceRegistrar.addStdResource(
-			XML_NAMESPACE_LIFERAY_DISPLAY_7_2_0, "/definitions/dtd/liferay-display_7_2_0.dtd",
-			LiferayDefinitionsResourceProvider.class);
+			XML_NAMESPACE_LIFERAY_DISPLAY_7_2_0, "/definitions/dtd/liferay-display_7_2_0.dtd", classLoader);
 		resourceRegistrar.addStdResource(
-			XML_NAMESPACE_LIFERAY_DISPLAY_7_3_0, "/definitions/dtd/liferay-display_7_3_0.dtd",
-			LiferayDefinitionsResourceProvider.class);
+			XML_NAMESPACE_LIFERAY_DISPLAY_7_3_0, "/definitions/dtd/liferay-display_7_3_0.dtd", classLoader);
 		resourceRegistrar.addStdResource(
-			XML_NAMESPACE_LIFERAY_DISPLAY_7_4_0, "/definitions/dtd/liferay-display_7_4_0.dtd",
-			LiferayDefinitionsResourceProvider.class);
+			XML_NAMESPACE_LIFERAY_DISPLAY_7_4_0, "/definitions/dtd/liferay-display_7_4_0.dtd", classLoader);
 		resourceRegistrar.addStdResource(
 			XML_NAMESPACE_LIFERAY_FRIENDLY_URL_ROUTES_7_0_0, "/definitions/dtd/liferay-friendly-url-routes_7_0_0.dtd",
-			LiferayDefinitionsResourceProvider.class);
+			classLoader);
 		resourceRegistrar.addStdResource(
 			XML_NAMESPACE_LIFERAY_FRIENDLY_URL_ROUTES_7_1_0, "/definitions/dtd/liferay-friendly-url-routes_7_1_0.dtd",
-			LiferayDefinitionsResourceProvider.class);
+			classLoader);
 		resourceRegistrar.addStdResource(
 			XML_NAMESPACE_LIFERAY_FRIENDLY_URL_ROUTES_7_2_0, "/definitions/dtd/liferay-friendly-url-routes_7_2_0.dtd",
-			LiferayDefinitionsResourceProvider.class);
+			classLoader);
 		resourceRegistrar.addStdResource(
 			XML_NAMESPACE_LIFERAY_FRIENDLY_URL_ROUTES_7_3_0, "/definitions/dtd/liferay-friendly-url-routes_7_3_0.dtd",
-			LiferayDefinitionsResourceProvider.class);
+			classLoader);
 		resourceRegistrar.addStdResource(
 			XML_NAMESPACE_LIFERAY_FRIENDLY_URL_ROUTES_7_4_0, "/definitions/dtd/liferay-friendly-url-routes_7_4_0.dtd",
-			LiferayDefinitionsResourceProvider.class);
+			classLoader);
 		resourceRegistrar.addStdResource(
-			XML_NAMESPACE_LIFERAY_HOOK_7_0_0, "/definitions/dtd/liferay-hook_7_0_0.dtd",
-			LiferayDefinitionsResourceProvider.class);
+			XML_NAMESPACE_LIFERAY_HOOK_7_0_0, "/definitions/dtd/liferay-hook_7_0_0.dtd", classLoader);
 		resourceRegistrar.addStdResource(
-			XML_NAMESPACE_LIFERAY_HOOK_7_1_0, "/definitions/dtd/liferay-hook_7_1_0.dtd",
-			LiferayDefinitionsResourceProvider.class);
+			XML_NAMESPACE_LIFERAY_HOOK_7_1_0, "/definitions/dtd/liferay-hook_7_1_0.dtd", classLoader);
 		resourceRegistrar.addStdResource(
-			XML_NAMESPACE_LIFERAY_HOOK_7_2_0, "/definitions/dtd/liferay-hook_7_2_0.dtd",
-			LiferayDefinitionsResourceProvider.class);
+			XML_NAMESPACE_LIFERAY_HOOK_7_2_0, "/definitions/dtd/liferay-hook_7_2_0.dtd", classLoader);
 		resourceRegistrar.addStdResource(
-			XML_NAMESPACE_LIFERAY_HOOK_7_3_0, "/definitions/dtd/liferay-hook_7_3_0.dtd",
-			LiferayDefinitionsResourceProvider.class);
+			XML_NAMESPACE_LIFERAY_HOOK_7_3_0, "/definitions/dtd/liferay-hook_7_3_0.dtd", classLoader);
 		resourceRegistrar.addStdResource(
-			XML_NAMESPACE_LIFERAY_HOOK_7_4_0, "/definitions/dtd/liferay-hook_7_4_0.dtd",
-			LiferayDefinitionsResourceProvider.class);
+			XML_NAMESPACE_LIFERAY_HOOK_7_4_0, "/definitions/dtd/liferay-hook_7_4_0.dtd", classLoader);
 
 		resourceRegistrar.addStdResource(
 			XML_NAMESPACE_LIFERAY_LAYOUT_TEMPLATES_7_0_0, "/definitions/dtd/liferay-layout-templates_7_0_0.dtd",
-			LiferayDefinitionsResourceProvider.class);
+			classLoader);
 		resourceRegistrar.addStdResource(
 			XML_NAMESPACE_LIFERAY_LAYOUT_TEMPLATES_7_1_0, "/definitions/dtd/liferay-layout-templates_7_1_0.dtd",
-			LiferayDefinitionsResourceProvider.class);
+			classLoader);
 		resourceRegistrar.addStdResource(
 			XML_NAMESPACE_LIFERAY_LAYOUT_TEMPLATES_7_2_0, "/definitions/dtd/liferay-layout-templates_7_2_0.dtd",
-			LiferayDefinitionsResourceProvider.class);
+			classLoader);
 		resourceRegistrar.addStdResource(
 			XML_NAMESPACE_LIFERAY_LAYOUT_TEMPLATES_7_3_0, "/definitions/dtd/liferay-layout-templates_7_3_0.dtd",
-			LiferayDefinitionsResourceProvider.class);
+			classLoader);
 		resourceRegistrar.addStdResource(
 			XML_NAMESPACE_LIFERAY_LAYOUT_TEMPLATES_7_4_0, "/definitions/dtd/liferay-layout-templates_7_4_0.dtd",
-			LiferayDefinitionsResourceProvider.class);
+			classLoader);
 
 		resourceRegistrar.addStdResource(
-			XML_NAMESPACE_LIFERAY_LOOK_AND_FEEL_7_0_0, "/definitions/dtd/liferay-look-and-feel_7_0_0.dtd",
-			LiferayDefinitionsResourceProvider.class);
+			XML_NAMESPACE_LIFERAY_LOOK_AND_FEEL_7_0_0, "/definitions/dtd/liferay-look-and-feel_7_0_0.dtd", classLoader);
 		resourceRegistrar.addStdResource(
-			XML_NAMESPACE_LIFERAY_LOOK_AND_FEEL_7_1_0, "/definitions/dtd/liferay-look-and-feel_7_1_0.dtd",
-			LiferayDefinitionsResourceProvider.class);
+			XML_NAMESPACE_LIFERAY_LOOK_AND_FEEL_7_1_0, "/definitions/dtd/liferay-look-and-feel_7_1_0.dtd", classLoader);
 		resourceRegistrar.addStdResource(
-			XML_NAMESPACE_LIFERAY_LOOK_AND_FEEL_7_2_0, "/definitions/dtd/liferay-look-and-feel_7_2_0.dtd",
-			LiferayDefinitionsResourceProvider.class);
+			XML_NAMESPACE_LIFERAY_LOOK_AND_FEEL_7_2_0, "/definitions/dtd/liferay-look-and-feel_7_2_0.dtd", classLoader);
 		resourceRegistrar.addStdResource(
-			XML_NAMESPACE_LIFERAY_LOOK_AND_FEEL_7_3_0, "/definitions/dtd/liferay-look-and-feel_7_3_0.dtd",
-			LiferayDefinitionsResourceProvider.class);
+			XML_NAMESPACE_LIFERAY_LOOK_AND_FEEL_7_3_0, "/definitions/dtd/liferay-look-and-feel_7_3_0.dtd", classLoader);
 		resourceRegistrar.addStdResource(
-			XML_NAMESPACE_LIFERAY_LOOK_AND_FEEL_7_4_0, "/definitions/dtd/liferay-look-and-feel_7_4_0.dtd",
-			LiferayDefinitionsResourceProvider.class);
+			XML_NAMESPACE_LIFERAY_LOOK_AND_FEEL_7_4_0, "/definitions/dtd/liferay-look-and-feel_7_4_0.dtd", classLoader);
 
 		resourceRegistrar.addStdResource(
 			XML_NAMESPACE_LIFERAY_PLUGIN_PACKAGE_7_0_0, "/definitions/dtd/liferay-plugin-package_7_0_0.dtd",
-			LiferayDefinitionsResourceProvider.class);
+			classLoader);
 		resourceRegistrar.addStdResource(
 			XML_NAMESPACE_LIFERAY_PLUGIN_PACKAGE_7_1_0, "/definitions/dtd/liferay-plugin-package_7_1_0.dtd",
-			LiferayDefinitionsResourceProvider.class);
+			classLoader);
 		resourceRegistrar.addStdResource(
 			XML_NAMESPACE_LIFERAY_PLUGIN_PACKAGE_7_2_0, "/definitions/dtd/liferay-plugin-package_7_2_0.dtd",
-			LiferayDefinitionsResourceProvider.class);
+			classLoader);
 		resourceRegistrar.addStdResource(
 			XML_NAMESPACE_LIFERAY_PLUGIN_PACKAGE_7_3_0, "/definitions/dtd/liferay-plugin-package_7_3_0.dtd",
-			LiferayDefinitionsResourceProvider.class);
+			classLoader);
 		resourceRegistrar.addStdResource(
 			XML_NAMESPACE_LIFERAY_PLUGIN_PACKAGE_7_4_0, "/definitions/dtd/liferay-plugin-package_7_4_0.dtd",
-			LiferayDefinitionsResourceProvider.class);
+			classLoader);
 		resourceRegistrar.addStdResource(
 			XML_NAMESPACE_LIFERAY_PLUGIN_REPOSITORY_7_0_0, "/definitions/dtd/liferay-plugin-repository_7_0_0.dtd",
-			LiferayDefinitionsResourceProvider.class);
+			classLoader);
 		resourceRegistrar.addStdResource(
 			XML_NAMESPACE_LIFERAY_PLUGIN_REPOSITORY_7_1_0, "/definitions/dtd/liferay-plugin-repository_7_1_0.dtd",
-			LiferayDefinitionsResourceProvider.class);
+			classLoader);
 		resourceRegistrar.addStdResource(
 			XML_NAMESPACE_LIFERAY_PLUGIN_REPOSITORY_7_2_0, "/definitions/dtd/liferay-plugin-repository_7_2_0.dtd",
-			LiferayDefinitionsResourceProvider.class);
+			classLoader);
 		resourceRegistrar.addStdResource(
 			XML_NAMESPACE_LIFERAY_PLUGIN_REPOSITORY_7_3_0, "/definitions/dtd/liferay-plugin-repository_7_3_0.dtd",
-			LiferayDefinitionsResourceProvider.class);
+			classLoader);
 		resourceRegistrar.addStdResource(
 			XML_NAMESPACE_LIFERAY_PLUGIN_REPOSITORY_7_4_0, "/definitions/dtd/liferay-plugin-repository_7_4_0.dtd",
-			LiferayDefinitionsResourceProvider.class);
+			classLoader);
 		resourceRegistrar.addStdResource(
-			XML_NAMESPACE_LIFERAY_PORTLET_APP_7_0_0, "/definitions/dtd/liferay-portlet-app_7_0_0.dtd",
-			LiferayDefinitionsResourceProvider.class);
+			XML_NAMESPACE_LIFERAY_PORTLET_APP_7_0_0, "/definitions/dtd/liferay-portlet-app_7_0_0.dtd", classLoader);
 		resourceRegistrar.addStdResource(
-			XML_NAMESPACE_LIFERAY_PORTLET_APP_7_1_0, "/definitions/dtd/liferay-portlet-app_7_1_0.dtd",
-			LiferayDefinitionsResourceProvider.class);
+			XML_NAMESPACE_LIFERAY_PORTLET_APP_7_1_0, "/definitions/dtd/liferay-portlet-app_7_1_0.dtd", classLoader);
 		resourceRegistrar.addStdResource(
-			XML_NAMESPACE_LIFERAY_PORTLET_APP_7_2_0, "/definitions/dtd/liferay-portlet-app_7_2_0.dtd",
-			LiferayDefinitionsResourceProvider.class);
+			XML_NAMESPACE_LIFERAY_PORTLET_APP_7_2_0, "/definitions/dtd/liferay-portlet-app_7_2_0.dtd", classLoader);
 		resourceRegistrar.addStdResource(
-			XML_NAMESPACE_LIFERAY_PORTLET_APP_7_3_0, "/definitions/dtd/liferay-portlet-app_7_3_0.dtd",
-			LiferayDefinitionsResourceProvider.class);
+			XML_NAMESPACE_LIFERAY_PORTLET_APP_7_3_0, "/definitions/dtd/liferay-portlet-app_7_3_0.dtd", classLoader);
 		resourceRegistrar.addStdResource(
-			XML_NAMESPACE_LIFERAY_PORTLET_APP_7_4_0, "/definitions/dtd/liferay-portlet-app_7_4_0.dtd",
-			LiferayDefinitionsResourceProvider.class);
+			XML_NAMESPACE_LIFERAY_PORTLET_APP_7_4_0, "/definitions/dtd/liferay-portlet-app_7_4_0.dtd", classLoader);
 		resourceRegistrar.addStdResource(
 			XML_NAMESPACE_LIFERAY_RESOURCE_ACTION_MAPPING_7_0_0,
-			"/definitions/dtd/liferay-resource-action-mapping_7_0_0.dtd", LiferayDefinitionsResourceProvider.class);
+			"/definitions/dtd/liferay-resource-action-mapping_7_0_0.dtd", classLoader);
 		resourceRegistrar.addStdResource(
 			XML_NAMESPACE_LIFERAY_RESOURCE_ACTION_MAPPING_7_1_0,
-			"/definitions/dtd/liferay-resource-action-mapping_7_1_0.dtd", LiferayDefinitionsResourceProvider.class);
+			"/definitions/dtd/liferay-resource-action-mapping_7_1_0.dtd", classLoader);
 		resourceRegistrar.addStdResource(
 			XML_NAMESPACE_LIFERAY_RESOURCE_ACTION_MAPPING_7_2_0,
-			"/definitions/dtd/liferay-resource-action-mapping_7_2_0.dtd", LiferayDefinitionsResourceProvider.class);
+			"/definitions/dtd/liferay-resource-action-mapping_7_2_0.dtd", classLoader);
 		resourceRegistrar.addStdResource(
 			XML_NAMESPACE_LIFERAY_RESOURCE_ACTION_MAPPING_7_3_0,
-			"/definitions/dtd/liferay-resource-action-mapping_7_3_0.dtd", LiferayDefinitionsResourceProvider.class);
+			"/definitions/dtd/liferay-resource-action-mapping_7_3_0.dtd", classLoader);
 		resourceRegistrar.addStdResource(
 			XML_NAMESPACE_LIFERAY_RESOURCE_ACTION_MAPPING_7_4_0,
-			"/definitions/dtd/liferay-resource-action-mapping_7_4_0.dtd", LiferayDefinitionsResourceProvider.class);
+			"/definitions/dtd/liferay-resource-action-mapping_7_4_0.dtd", classLoader);
 
 		resourceRegistrar.addStdResource(
 			XML_NAMESPACE_LIFERAY_SERVICE_BUILDER_7_0_0, "/definitions/dtd/liferay-service-builder_7_0_0.dtd",
-			LiferayDefinitionsResourceProvider.class);
+			classLoader);
 		resourceRegistrar.addStdResource(
 			XML_NAMESPACE_LIFERAY_SERVICE_BUILDER_7_1_0, "/definitions/dtd/liferay-service-builder_7_1_0.dtd",
-			LiferayDefinitionsResourceProvider.class);
+			classLoader);
 		resourceRegistrar.addStdResource(
 			XML_NAMESPACE_LIFERAY_SERVICE_BUILDER_7_2_0, "/definitions/dtd/liferay-service-builder_7_2_0.dtd",
-			LiferayDefinitionsResourceProvider.class);
+			classLoader);
 		resourceRegistrar.addStdResource(
 			XML_NAMESPACE_LIFERAY_SERVICE_BUILDER_7_3_0, "/definitions/dtd/liferay-service-builder_7_3_0.dtd",
-			LiferayDefinitionsResourceProvider.class);
+			classLoader);
 		resourceRegistrar.addStdResource(
 			XML_NAMESPACE_LIFERAY_SERVICE_BUILDER_7_4_0, "/definitions/dtd/liferay-service-builder_7_4_0.dtd",
-			LiferayDefinitionsResourceProvider.class);
+			classLoader);
 		resourceRegistrar.addStdResource(
-			XML_NAMESPACE_LIFERAY_SOCIAL_7_0_0, "/definitions/dtd/liferay-social_7_0_0.dtd",
-			LiferayDefinitionsResourceProvider.class);
+			XML_NAMESPACE_LIFERAY_SOCIAL_7_0_0, "/definitions/dtd/liferay-social_7_0_0.dtd", classLoader);
 		resourceRegistrar.addStdResource(
-			XML_NAMESPACE_LIFERAY_SOCIAL_7_1_0, "/definitions/dtd/liferay-social_7_1_0.dtd",
-			LiferayDefinitionsResourceProvider.class);
+			XML_NAMESPACE_LIFERAY_SOCIAL_7_1_0, "/definitions/dtd/liferay-social_7_1_0.dtd", classLoader);
 		resourceRegistrar.addStdResource(
-			XML_NAMESPACE_LIFERAY_SOCIAL_7_2_0, "/definitions/dtd/liferay-social_7_2_0.dtd",
-			LiferayDefinitionsResourceProvider.class);
+			XML_NAMESPACE_LIFERAY_SOCIAL_7_2_0, "/definitions/dtd/liferay-social_7_2_0.dtd", classLoader);
 		resourceRegistrar.addStdResource(
-			XML_NAMESPACE_LIFERAY_SOCIAL_7_3_0, "/definitions/dtd/liferay-social_7_3_0.dtd",
-			LiferayDefinitionsResourceProvider.class);
+			XML_NAMESPACE_LIFERAY_SOCIAL_7_3_0, "/definitions/dtd/liferay-social_7_3_0.dtd", classLoader);
 		resourceRegistrar.addStdResource(
-			XML_NAMESPACE_LIFERAY_SOCIAL_7_4_0, "/definitions/dtd/liferay-social_7_4_0.dtd",
-			LiferayDefinitionsResourceProvider.class);
+			XML_NAMESPACE_LIFERAY_SOCIAL_7_4_0, "/definitions/dtd/liferay-social_7_4_0.dtd", classLoader);
 		resourceRegistrar.addStdResource(
-			XML_NAMESPACE_LIFERAY_THEME_LOADER_7_0_0, "/definitions/dtd/liferay-theme-loader_7_0_0.dtd",
-			LiferayDefinitionsResourceProvider.class);
+			XML_NAMESPACE_LIFERAY_THEME_LOADER_7_0_0, "/definitions/dtd/liferay-theme-loader_7_0_0.dtd", classLoader);
 		resourceRegistrar.addStdResource(
-			XML_NAMESPACE_LIFERAY_THEME_LOADER_7_1_0, "/definitions/dtd/liferay-theme-loader_7_1_0.dtd",
-			LiferayDefinitionsResourceProvider.class);
+			XML_NAMESPACE_LIFERAY_THEME_LOADER_7_1_0, "/definitions/dtd/liferay-theme-loader_7_1_0.dtd", classLoader);
 		resourceRegistrar.addStdResource(
-			XML_NAMESPACE_LIFERAY_THEME_LOADER_7_2_0, "/definitions/dtd/liferay-theme-loader_7_2_0.dtd",
-			LiferayDefinitionsResourceProvider.class);
+			XML_NAMESPACE_LIFERAY_THEME_LOADER_7_2_0, "/definitions/dtd/liferay-theme-loader_7_2_0.dtd", classLoader);
 		resourceRegistrar.addStdResource(
-			XML_NAMESPACE_LIFERAY_THEME_LOADER_7_3_0, "/definitions/dtd/liferay-theme-loader_7_3_0.dtd",
-			LiferayDefinitionsResourceProvider.class);
+			XML_NAMESPACE_LIFERAY_THEME_LOADER_7_3_0, "/definitions/dtd/liferay-theme-loader_7_3_0.dtd", classLoader);
 		resourceRegistrar.addStdResource(
-			XML_NAMESPACE_LIFERAY_THEME_LOADER_7_4_0, "/definitions/dtd/liferay-theme-loader_7_4_0.dtd",
-			LiferayDefinitionsResourceProvider.class);
+			XML_NAMESPACE_LIFERAY_THEME_LOADER_7_4_0, "/definitions/dtd/liferay-theme-loader_7_4_0.dtd", classLoader);
 		resourceRegistrar.addStdResource(
 			XML_NAMESPACE_LIFERAY_USER_NOTIFICATION_DEFINITIONS_7_0_0,
-			"/definitions/dtd/liferay-user-notification-definitions_7_0_0.dtd",
-			LiferayDefinitionsResourceProvider.class);
+			"/definitions/dtd/liferay-user-notification-definitions_7_0_0.dtd", classLoader);
 		resourceRegistrar.addStdResource(
 			XML_NAMESPACE_LIFERAY_USER_NOTIFICATION_DEFINITIONS_7_1_0,
-			"/definitions/dtd/liferay-user-notification-definitions_7_1_0.dtd",
-			LiferayDefinitionsResourceProvider.class);
+			"/definitions/dtd/liferay-user-notification-definitions_7_1_0.dtd", classLoader);
 		resourceRegistrar.addStdResource(
 			XML_NAMESPACE_LIFERAY_USER_NOTIFICATION_DEFINITIONS_7_2_0,
-			"/definitions/dtd/liferay-user-notification-definitions_7_2_0.dtd",
-			LiferayDefinitionsResourceProvider.class);
+			"/definitions/dtd/liferay-user-notification-definitions_7_2_0.dtd", classLoader);
 		resourceRegistrar.addStdResource(
 			XML_NAMESPACE_LIFERAY_USER_NOTIFICATION_DEFINITIONS_7_3_0,
-			"/definitions/dtd/liferay-user-notification-definitions_7_3_0.dtd",
-			LiferayDefinitionsResourceProvider.class);
+			"/definitions/dtd/liferay-user-notification-definitions_7_3_0.dtd", classLoader);
 		resourceRegistrar.addStdResource(
 			XML_NAMESPACE_LIFERAY_USER_NOTIFICATION_DEFINITIONS_7_4_0,
-			"/definitions/dtd/liferay-user-notification-definitions_7_4_0.dtd",
-			LiferayDefinitionsResourceProvider.class);
+			"/definitions/dtd/liferay-user-notification-definitions_7_4_0.dtd", classLoader);
 		resourceRegistrar.addStdResource(
 			XML_NAMESPACE_LIFERAY_WORKFLOW_DEFINITION_7_0_0, "/definitions/xsd/liferay-workflow-definition_7_0_0.xsd",
-			LiferayDefinitionsResourceProvider.class);
+			classLoader);
 		resourceRegistrar.addStdResource(
 			XML_NAMESPACE_LIFERAY_WORKFLOW_DEFINITION_7_1_0, "/definitions/xsd/liferay-workflow-definition_7_1_0.xsd",
-			LiferayDefinitionsResourceProvider.class);
+			classLoader);
 		resourceRegistrar.addStdResource(
 			XML_NAMESPACE_LIFERAY_WORKFLOW_DEFINITION_7_2_0, "/definitions/xsd/liferay-workflow-definition_7_2_0.xsd",
-			LiferayDefinitionsResourceProvider.class);
+			classLoader);
 		resourceRegistrar.addStdResource(
 			XML_NAMESPACE_LIFERAY_WORKFLOW_DEFINITION_7_3_0, "/definitions/xsd/liferay-workflow-definition_7_3_0.xsd",
-			LiferayDefinitionsResourceProvider.class);
+			classLoader);
 		resourceRegistrar.addStdResource(
 			XML_NAMESPACE_LIFERAY_WORKFLOW_DEFINITION_7_4_0, "/definitions/xsd/liferay-workflow-definition_7_4_0.xsd",
-			LiferayDefinitionsResourceProvider.class);
+			classLoader);
 
 		resourceRegistrar.addStdResource(
-			XML_NAMESPACE_SUN_PORTLET_APP_2_0, "/definitions/xsd/portlet-app_2_0.xsd",
-			LiferayDefinitionsResourceProvider.class);
+			XML_NAMESPACE_SUN_PORTLET_APP_2_0, "/definitions/xsd/portlet-app_2_0.xsd", classLoader);
 		resourceRegistrar.addStdResource(
-			XML_NAMESPACE_JCP_PORTLET_APP_3_0, "/definitions/xsd/portlet-app_3_0.xsd",
-			LiferayDefinitionsResourceProvider.class);
+			XML_NAMESPACE_JCP_PORTLET_APP_3_0, "/definitions/xsd/portlet-app_3_0.xsd", classLoader);
 	}
 
 }
